a7b60268c157b4f73b18070264711adc48c565f4
2226j1526 <aakash33507@gmail.com>
2024-12-09 15:27:19 +0530

    images updates

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -3,6 +3,13 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import img2 from './Asset/gt.jpg';
 import Placeholder from 'react-bootstrap/Placeholder';
+import img3 from './Asset/st.png';
+import img4 from './Asset/bb.jpg';
+import img5 from './Asset/sg.jpg';
+import img6 from './Asset/wd.jpg';
+import img7 from './Asset/mf.jpg';
+import img8 from './Asset/goat.jpeg';
+
 
 export const Grid = () => {
   return (
@@ -11,48 +18,44 @@ export const Grid = () => {
    <div className='banner'>
    <div className='cont'>
      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img3} />
       <Card.Body>
      
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Stranger Things</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Small-town kids battle otherworldly creatures after their friend vanishes in a mysterious government experiment.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img4} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Breaking Bad</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        A chemistry teacher diagnosed with cancer turns to a life of crime to secure his family's future, spiraling into a dangerous world.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img5} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Squid Game</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Desperate contestants compete in deadly children's games for a massive cash prize in this dark and satirical South Korean thriller.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img6} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>Wednesday</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        Addams's daughter investigates a mystery at a gothic school, showcasing her dark humor and psychic abilities.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
@@ -63,19 +66,18 @@ export const Grid = () => {
     <h3 className='text-white'>&nbsp;&nbsp; Continue Watching &gt;&gt;</h3>
     <div className='cont1'>
      <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img8} />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>The Greatest of All Time</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        A time-traveling action epic where a legendary athlete faces off against a younger, AI-enhanced version of themselves.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
     </Card>
 
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={img2} />
+      <Card.Img variant="top" src={img7} />
       <Card.Body>
       <Placeholder as={Card.Title} animation="glow">
             <Placeholder xs={6} />
